b6c0aa32be968625cadba0784c4f8f219d5bc9f3
Hugo Laliberté <hugolaliberte@gmail.com>
2015-12-13 19:21:40 -0500

    Backup before we destroy everything once again

--- OctoWS2811Demo-udp-backup/OctoWS2811Demo-udp-backup.ino
+++ OctoWS2811Demo-udp-backup/OctoWS2811Demo-udp-backup.ino
@@ -7,8 +7,7 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
 
-//#define UDP_TX_PACKET_MAX_SIZE 6020
-#define UDP_TX_PACKET_MAX_SIZE 10000
+#define UDP_TX_PACKET_MAX_SIZE 6020
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -16,25 +15,21 @@ byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(10, 0, 1, 155);
 
-//unsigned int champiPort1 = 9151;      // Plafond part 1
-//unsigned int champiPort2 = 9152;      // Plafond part 1
-//unsigned int champiPort3 = 9153;      // Plafond part 1
-unsigned int plafondPort1 = 9155;      // Plafond part 1
-unsigned int plafondPort2 = 9165;      // Plafond part 2
+unsigned int plafondPort1 = 9155;      // local port to listen on
+unsigned int plafondPort2 = 9165;      // local port to listen on
 
 // buffers for receiving and sending data
-//char packetBufferC1[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
-char packetBuffer2[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+char packetBufferP1[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+char packetBufferP2[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
 //char  ReplyBuffer[] = "acknowledged";       // a string to send back
 
 // An EthernetUDP instance to let us send and receive packets over UDP
-//EthernetUDP tabarnac;
 EthernetUDP UdpP1;
 EthernetUDP UdpP2;
 
 #define NUM_LEDS_PER_STRIP 504
 #define NUM_STRIPS 4
+
 
 CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
@@ -46,7 +41,6 @@ void setup() {
   LEDS.setBrightness(33);
   // start the Ethernet and UDP:
   Ethernet.begin(mac,ip);
-//  tabarnac.begin(champiPort1);
   UdpP1.begin(plafondPort1);
   UdpP2.begin(plafondPort2);
   Serial.begin(115200);
@@ -54,28 +48,32 @@ void setup() {
 }
 
 void loop() {
-//  int fuckYou = UdpC1.parsePacket()
-  int packetSize = UdpP1.parsePacket();
-  int packetSize2 = UdpP2.parsePacket();
-  if(packetSize)
+  int packetSizeP1 = UdpP1.parsePacket();
+  int packetSizeP2 = UdpP2.parsePacket();
+  if(packetSizeP1)
   {
-    UdpP1.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
+    UdpP1.read(packetBufferP1,UDP_TX_PACKET_MAX_SIZE);
   }
-  if(packetSize2)
+  if(packetSizeP2)
   {
-    UdpP2.read(packetBuffer2,UDP_TX_PACKET_MAX_SIZE);
+    UdpP2.read(packetBufferP2,UDP_TX_PACKET_MAX_SIZE);
   }
-  Serial.println(packetBuffer);
+  Serial.println(packetBufferP1);
+//  Serial.print("packetBuffer: ");
+//  Serial.println(packetBuffer[0] + packetBuffer[1] + packetBuffer[2]);
+//  static uint8_t hue = 0;
   for(int i = 0; i < NUM_STRIPS; i++) {
     for(int j = 0; j < NUM_LEDS_PER_STRIP; j++) {
-//      leds[(i*NUM_LEDS_PER_STRIP) + j] = CRGB(255, 255 ,255);
-     
       if(j < 490)
-        leds[(i*NUM_LEDS_PER_STRIP) + j] = CRGB(packetBuffer[(j*3)],packetBuffer[(j*3)+1],packetBuffer[(j*3)+2]);
+        leds[(i*NUM_LEDS_PER_STRIP) + j] = CRGB(packetBufferP1[(j*3)],packetBufferP1[(j*3)+1],packetBufferP1[(j*3)+2]);
       else
-        leds[(i*NUM_LEDS_PER_STRIP) + j] = CRGB(packetBuffer2[(j*3) - UDP_TX_PACKET_MAX_SIZE],packetBuffer2[((j*3)+1) - UDP_TX_PACKET_MAX_SIZE],packetBuffer2[((j*3)+2) - UDP_TX_PACKET_MAX_SIZE]);
+        leds[(i*NUM_LEDS_PER_STRIP) + j] = CRGB(packetBufferP2[(j*3)],packetBufferP2[(j*3)+1],packetBufferP2[(j*3)+2]);
+
+//        leds[(i*NUM_LEDS_PER_STRIP) + j] = CRGB(255,0,255);
     }
   }
+      
   LEDS.show();
   delay(50);
+//  LEDS.delay(10);
 }
---
+++ testlistenport/testlistenport.ino
@@ -0,0 +1,32 @@
+#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <Ethernet.h>
+#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+
+#define UDP_TX_PACKET_MAX_SIZE 6020
+
+byte mac[] = {  
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(10, 0, 1, 155);
+
+unsigned int localPort = 9155;
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+void setup() {
+
+  Ethernet.begin(mac,ip);
+  Udp.begin(localPort);
+  Serial.begin(115200);
+  Serial.println("INIT");
+}
+
+void loop() {
+  int packetSize = Udp.parsePacket();
+    
+  if(packetSize)
+  {
+    Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
+  }
+  Serial.println(packetBuffer);
+}
